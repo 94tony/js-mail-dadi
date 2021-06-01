f8d1e8a0ea67988a5a834c816ee82c79724f9502
94tony <dagatatony94@gmail.com>
2021-06-01 17:42:41 +0200

    completo

--- jv/script.js
+++ jv/script.js
@@ -1,7 +1,7 @@
 var mail = prompt("qual'è la tua email?") ;
 var registrati = ['Pippo@gmail.com', 'Luca@gmail.com', 'Paolo@gmail.com', 'Marco@gmail.com', 'Maria@gmail.com', 'Gianni@gmail.com'];
 
-var output = document.getElementById('registrati-list');
+var output = document.getElementById('result');
 
 var check = false; // deve diventare true solo se trovo anna.
 
@@ -12,9 +12,21 @@ for (var i = 0; i < registrati.length; i++) {
     }
 }
 
-if (check === true) {
+if (check) {
     output.innerHTML += "Ho trovato l'email !";
 } else {
     output.innerHTML += "Non ho trovato l'email";
 }
 
+
+var userNumber = Math.floor(Math.random() *6) +1 ;
+var cpuNumber = Math.floor(Math.random()* 6) +1;
+document.getElementById('result1').innerHTML += '<br/>' + userNumber + ' ' + cpuNumber + '<br/>' ;
+if(userNumber > cpuNumber) {
+    document.getElementById('result1').innerHTML += ' hai vinto' +  ;
+}else if (userNumber < cpuNumber) {
+    document.getElementById('result1').innerHTML += ' il computer ha vinto';
+}else{
+    document.getElementById('result1').innerHTML += ' pari';
+    
+}
